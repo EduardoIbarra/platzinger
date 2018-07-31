892f8914efaa9cc71ac910c65b0199592429e377
Eduardo Ibarra <eduardoibara904@gmail.com>
2018-07-31 09:08:31 -0500

    estilos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  operation: string = 'login';
   constructor() { }
 
   ngOnInit() {
